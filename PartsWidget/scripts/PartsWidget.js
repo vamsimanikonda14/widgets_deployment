a450d0b7a0b9befee0a17ffbee627e6844802ff6
vamsimanikonda14 <vamsi.manikonda@skyronsoftware.com>
2025-02-19 11:35:00 +0530

    Auto commit: Files copied and updated

--- PartsWidget/scripts/PartsWidget.js
+++ PartsWidget/scripts/PartsWidget.js
@@ -1,9 +1,10 @@
 define("DS/PartsWidget/scripts/PartsWidget", ["DS/WAFData/WAFData"], function (WAFData) {
     'use strict';
+
     var myWidget = {
         onLoad: function () {
-           // this.WAFData = WAFData;
-            widget.body.innerHTML =  
+            // Adding a button and table container
+            document.body.innerHTML =  
                 "<button id='partsButton'>Parts</button>" + // Add button
                 "<div id='tableContainer'></div>"; // Container for table
 
@@ -13,50 +14,43 @@ define("DS/PartsWidget/scripts/PartsWidget", ["DS/WAFData/WAFData"], function (W
             });
         },
 
-        fetchData: function() {
-             
-                    let dataSetSpaceUrl =  "https://vdemopro1161dsy.extranet.3ds.com/3DSpace"+"/resources/v1/modeler/dseng/dseng:EngItem/search";
-                    var url = dataSetSpaceUrl+ "?$searchStr=VPMReference&$mask=dsmveng:EngItemMask.Details" ;
-                    var request = url;
+        fetchData: function () {
+            let dataSetSpaceUrl = "https://vdemopro1161dsy.extranet.3ds.com/3DSpace/resources/v1/modeler/dseng/dseng:EngItem/search";
+            var url = dataSetSpaceUrl + "?$searchStr=VPMReference&$mask=dsmveng:EngItemMask.Details";
     
-            //let that = this;
-            WAFData.authenticatedRequest(request, {
+            WAFData.authenticatedRequest(url, {
                 method: "GET",
-              //  data: JSON.stringify(_data),
                 headers: {
                     "Accept": "*/*",
                     "Content-Type": "application/json",
-                   // "Cache-Control": "no-cache",
                     "SecurityContext": "VPLMAdmin.Company Name.Default"
                 },
                 type: 'json',
                 onComplete: function (data) {
-                    console.log("On complete",data);
-                    myWidget.renderTable(data);
+                    console.log("On complete", data);
+                    if (data && data.member) {
+                        myWidget.renderTable(data.member); // Ensure data.member exists
+                    }
                 },
                 onFailure: function (error) {
-                    console.log("fail: " + error);
+                    console.log("Request failed:", error);
                 }
-            }); 
-    
-    
-     
-    },
+            });
+        },
 
         renderTable: function (data) {
-            // Dynamically generate a table based on the fetched data
             var tableHtml = "<table border='1'>" +
                             "<thead>" +
                             "<tr><th>Type</th><th>Name</th><th>Revision</th><th>Maturity</th></tr>" + // Table headers
                             "</thead><tbody>";
 
             // Loop through the API data to generate table rows
-            data.member.forEach(function (part) {
+            data.forEach(function (part) {
                 tableHtml += "<tr>" +
-                             "<td>" + part.type + "</td>" +
-                             "<td>" + part.name + "</td>" +
-                             "<td>" + part.revision + "</td>" +
-                             "<td>" + part.state + "</td>" +
+                             "<td>" + (part.type || 'N/A') + "</td>" + // Handle missing values
+                             "<td>" + (part.name || 'N/A') + "</td>" + // Handle missing values
+                             "<td>" + (part.revision || 'N/A') + "</td>" + // Handle missing values
+                             "<td>" + (part.state || 'N/A') + "</td>" + // Handle missing values
                              "</tr>";
             });
 
@@ -67,5 +61,6 @@ define("DS/PartsWidget/scripts/PartsWidget", ["DS/WAFData/WAFData"], function (W
         }
     };
 
-    widget.addEvent('onLoad', myWidget.onLoad);
+    // Initialize the widget when it's ready
+    return myWidget;
 });

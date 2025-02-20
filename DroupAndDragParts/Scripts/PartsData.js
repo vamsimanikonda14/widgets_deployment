6c0b6cd503d80152576b3255f048e6c318adb1cc
vamsimanikonda14 <vamsi.manikonda@skyronsoftware.com>
2025-02-20 16:05:00 +0530

    Auto commit: Files copied and updated

--- DroupAndDragParts/Scripts/PartsData.js
+++ DroupAndDragParts/Scripts/PartsData.js
@@ -6,7 +6,7 @@ define("DS/DroupAndDragParts/Scripts/PartsData", ["DS/DataDragAndDrop/DataDragAn
         
         onLoad: function () {
             var html_before_drop = "<div class='main-Container' id='mainContainer'>"+
-            <h1>Droup here</h1>+"</div>";
+            "<h1>Droup here</h1>"+"</div>";
             widget.body.innerHTML=html_before_drop ;
             var theDropElt = document.querySelector('#mainContainer');
 

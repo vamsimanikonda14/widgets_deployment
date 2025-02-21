8f66dab4ff33d3254b7a71a4ff3f2dcd53bd0cf3
vamsimanikonda14 <vamsi.manikonda@skyronsoftware.com>
2025-02-21 17:05:00 +0530

    Auto commit: Files copied and updated

--- publishAndSubscribe/scripts/Subscribe.js
+++ publishAndSubscribe/scripts/Subscribe.js
@@ -3,21 +3,17 @@ define("DS/PartsWidget/scripts/SubscriberWidget", [], function () {
 
     var subscriberWidget = {
         onLoad: function () {
-            // Add a container to show the message or data
-            document.body.innerHTML = "<div id='subscriberData'>Waiting for data...</div>";
-            
-            // Subscribe to the "partsDataUpdated" event
-            window.addEventListener("partsDataUpdated", function (event) {
-                subscriberWidget.updateData(event.detail); // Handle the data when received
+               var sub ;
+     
+		sub = API.subscribe("publishedData", function (data) {
+       //Do the work ...  
+		console.log('Received from: ',data);
+      
+       // Unsubscribing sub to avoid getting more messages
+		API.unsubscribe(sub);
+    });
             });
         },
 
-        updateData: function (data) {
-            var subscriberDataDiv = document.getElementById('subscriberData');
-            subscriberDataDiv.innerHTML = "<strong>Received Data:</strong><pre>" + JSON.stringify(data, null, 2) + "</pre>";
-        }
-    };
-
-    // Return the widget object to be used in the main script
-    return subscriberWidget;
+         
 });
